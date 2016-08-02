1bc49711ce02cbbcf1b12ac09ee1a0b48f0e6f6a
Cyriac Thomas <cyriac@compile.com>
2016-08-02 17:26:42 +0530

    moving to class based approach so that fetching and parsing are independent

--- README.md
+++ README.md
@@ -4,15 +4,11 @@ A simple html scraper with xpath.
 
 ## Usage
 ```
-import hodor
+from hodor import Hodor
 config = {'src': '/html/body/div[1]/nav/div/div[1]/a/img/@src',
           'width': '/html/body/div[1]/nav/div/div[1]/a/img/@width'}
-hodor.get(url='https://www.compile.com', config=config)
-```
-
-## Result
-```
-{'src': ['/img/compile-logo-white.1002b288.svg'], 'width': ['100px']}
+h = Hodor(url='https://www.compile.com', config=config)
+h.data
 ```
 
 It also takes arguments:
@@ -20,5 +16,9 @@ It also takes arguments:
 - ```proxies``` (check requesocks)
 - ```auth```
 
+## Result
+```
+{'src': ['/img/compile-logo-white.1002b288.svg'], 'width': ['100px']}
+```
 
 ![DO one thing](https://pbs.twimg.com/media/CjSN_N5XIAEmnc0.jpg)
--- hodor/__init__.py
+++ hodor/__init__.py
@@ -1,23 +1,48 @@
 from lxml import html
 import requesocks
 
-def get_value(xpath, content):
-    tree = html.fromstring(content)
-    return tree.xpath(xpath)
+class Hodor(object):
+    def __init__(self, url, config={}, proxies={}, auth=None):
+        self.content = None
+        self.url = url
+        self.config = config
+        self.proxies = proxies
+        self.auth = auth
 
-def get(url, config={}, proxies={}, auth=None):
-    session = requesocks.session()
-    if len(proxies) > 0:
-        session.proxies = proxies
-    if auth:
-        r = session.get(url, auth=auth)
-    else:
-        r = session.get(url)
-    content = r.content
-    if len(config) is 0:
-        data = {'content': content}
-    else:
-        data = {}
-        for key, xpath in config.items():
-            data[key] = get_value(xpath, content)
-    return data
+    def fetch(self):
+        '''Does the requests fetching and stores result in self.content'''
+        session = requesocks.session()
+        if len(self.proxies) > 0:
+            session.proxies = proxies
+        if self.auth:
+            r = session.get(self.url, auth=self.auth)
+        else:
+            r = session.get(self.url)
+        self.content = r.content
+        return self.content
+
+    def get_value(self, xpath):
+        '''Returns result for a specific xpath'''
+        tree = html.fromstring(self.content)
+        return tree.xpath(xpath)
+
+    def parse(self):
+        '''Parses the content based on the config set and stores in _data'''
+        if self.content is None:
+            self.fetch()
+        if len(self.config) is 0:
+            self._data = {'content': self.content}
+        else:
+            self._data = {}
+            for key, xpath in self.config.items():
+                self._data[key] = self.get_value(xpath)
+        return self._data
+
+    def get(self):
+        return self.parse()
+
+    @property
+    def data(self):
+        if not hasattr(self, '_data'):
+            self.parse()
+        return self._data

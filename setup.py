1c8de39bc6930ad410e82cb2f127ef0a125f4057
Cyriac Thomas <cyriac@compile.com>
2021-10-11 11:00:00 +0530

    updated version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def parse_requirements(filename):
 
 
 install_reqs = parse_requirements('requirements.txt')
-version = '1.2.6'
+version = '1.2.8'
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(

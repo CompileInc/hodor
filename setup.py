72053abd15d63233dcf34976f4a6384f267f2f0d
Cyriac Thomas <cyriac@compile.com>
2023-11-06 12:39:45 +0530

    Upgrade requests

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def parse_requirements(filename):
 
 
 install_reqs = parse_requirements('requirements.txt')
-version = '1.2.9'
+version = '1.2.10'
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(

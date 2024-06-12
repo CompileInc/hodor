b12828d8e28f56690d41884dc4db30af6f17b15e
Cyriac Thomas <cyriac@compile.com>
2024-06-12 18:03:34 +0400

    Fix build deprecations and use pypa/build

--- upload.sh
+++ upload.sh
@@ -2,6 +2,7 @@ set -peu  # fail on first error
 
 pandoc --from=markdown --to=rst --output=README.txt README.md
 rm -rf dist
-python setup.py sdist bdist_wheel
+# requires pypa/build
+python -m build
 twine upload -r testpypi dist/*
 twine upload -r pypi dist/*
